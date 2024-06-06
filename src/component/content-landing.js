1004998159f1176f2f6881c1713ddca191c6bc6b
jihan <jihandatuslanibrahim@gmail.com>
2024-06-06 21:38:31 +0700

    Memperbaiki tata letak landing page pada tampilan desktop

--- src/component/content-landing.js
+++ src/component/content-landing.js
@@ -1,152 +1,155 @@
 class contentLanding extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    connectedCallback() {
-        this.render();
-        this.fetchData();
-    }
+  connectedCallback() {
+    this.render();
+    this.fetchData();
+  }
 
-    render() {
+  render() {
     this.shadowRoot.innerHTML = `
     <style>@import "/src/styles/tailwindcss-output/tailwind.css";</style>
 
-    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 bg-orange-600/65 mt-20 mb-20 rounded-3xl">
-        <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-            <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-                <img
-                alt=""
-                src="../assets/image/BROMO.jpg"
-                class="absolute inset-0 h-full w-full object-cover"
-                />
-            </div>
-
-            <div class="lg:py-24">
-                <h2 class="text-3xl font-bold sm:text-4xl">BROMO</h2>
-
-                <p class="mt-4 text-gray-50"></p>
-
-                <a
-                href="#"
-                class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-                >
-                Explore Now!
-                </a>
-            </div>
+        <div
+          class="grid grid-cols-1 gap-6 my-12 sm:grid-cols-2 bg-[#436850] rounded-[1.875rem] w-full mx-auto">
+          <div>
+            <img
+              src="../assets/image/gambar-pemandangan-indonesia.jpg"
+              alt=""
+              class="w-full h-full object-cover  rounded-l-[1.875rem]" />
+          </div>
+          <div class="p-5">
+            <h2 class="md:text-2xl md:pb-6 font-bold sm:text-xl text-white">BROMO</h2>
+            <p class="mb-3 text-white font-light ">
+            Deliver great service experiences fast - without the complexity of
+            traditional ITSM solutions.Accelerate critical development work,
+            eliminate toil, and deploy changes with ease, with a complete audit
+            trail for every change.
+            </p>
+            <a
+              href="#"
+              class=" my-4 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                ">
+              Explore Now!
+            </a>
+          </div>
         </div>
-    </div>
 
-    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 bg-orange-600/65 mt-20 mb-20 rounded-3xl">
-        <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-            <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-first lg:h-full">
-                <img
-                alt=""
-                src="../assets/image/BROMO.jpg"
-                class="absolute inset-0 h-full w-full object-cover"
-                />
-            </div>
-
-            <div class="lg:py-24">
-                <h2 class="text-3xl font-bold sm:text-4xl">BROMO</h2>
-
-                <p class="mt-4 text-gray-50"></p>
-
-                <a
-                href="#"
-                class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-                >
-                Explore Now!
-                </a>
-            </div>
+        <div class="grid grid-cols-1 gap-6 my-12 sm:grid-cols-2 bg-[#436850] rounded-[1.875rem] w-full mx-auto">
+          <div class="p-5">
+            <h2 class="md:text-2xl md:pb-6 font-bold sm:text-xl text-white">BROMO</h2>
+            <p class="mb-3 text-white font-light ">
+            Deliver great service experiences fast - without the complexity of
+            traditional ITSM solutions.Accelerate critical development work,
+            eliminate toil, and deploy changes with ease, with a complete audit
+            trail for every change.
+            </p>
+            <a
+              href="#"
+              class=" my-4 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                ">
+              Explore Now!
+            </a>
+          </div>
+            <div>
+            <img
+              src="../assets/image/gambar-pemandangan-indonesia.jpg"
+              alt=""
+              class="w-full h-full object-cover  rounded-r-[1.875rem]" />
+          </div>
         </div>
-    </div>
 
-    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 bg-orange-600/65 mt-20 mb-20 rounded-3xl">
-        <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-            <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-                <img
-                alt=""
-                src="../assets/image/BROMO.jpg"
-                class="absolute inset-0 h-full w-full object-cover"
-                />
-            </div>
 
-            <div class="lg:py-24">
-                <h2 class="text-3xl font-bold sm:text-4xl">BROMO</h2>
-
-                <p class="mt-4 text-gray-50"></p>
-
-                <a
-                href="#"
-                class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-                >
-                Explore Now!
-                </a>
-            </div>
+    <div
+          class="grid grid-cols-1 gap-6 my-12 sm:grid-cols-2 bg-[#436850] rounded-[1.875rem] w-full mx-auto">
+          <div>
+            <img
+              src="../assets/image/gambar-pemandangan-indonesia.jpg"
+              alt=""
+              class="w-full h-full object-cover  rounded-l-[1.875rem]" />
+          </div>
+          <div class="p-5">
+            <h2 class="md:text-2xl md:pb-6 font-bold sm:text-xl text-white">BROMO</h2>
+            <p class="mb-3 text-white font-light ">
+            Deliver great service experiences fast - without the complexity of
+            traditional ITSM solutions.Accelerate critical development work,
+            eliminate toil, and deploy changes with ease, with a complete audit
+            trail for every change.
+            </p>
+            <a
+              href="#"
+              class=" my-4 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                ">
+              Explore Now!
+            </a>
+          </div>
         </div>
-    </div>
 
-    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 bg-orange-600/65 mt-20 mb-20 rounded-3xl">
-        <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-            <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-first lg:h-full">
-                <img
-                alt=""
-                src="../assets/image/BROMO.jpg"
-                class="absolute inset-0 h-full w-full object-cover"
-                />
-            </div>
-
-            <div class="lg:py-24">
-                <h2 class="text-3xl font-bold sm:text-4xl">BROMO</h2>
-
-                <p class="mt-4 text-gray-50"></p>
-
-                <a
-                href="#"
-                class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-                >
-                Explore Now!
-                </a>
-            </div>
+       <div class="grid grid-cols-1 gap-6 my-12 sm:grid-cols-2 bg-[#436850] rounded-[1.875rem] w-full mx-auto">
+          <div class="p-5">
+            <h2 class="md:text-2xl md:pb-6 font-bold sm:text-xl text-white">BROMO</h2>
+            <p class="mb-3 text-white font-light ">
+            Deliver great service experiences fast - without the complexity of
+            traditional ITSM solutions.Accelerate critical development work,
+            eliminate toil, and deploy changes with ease, with a complete audit
+            trail for every change.
+            </p>
+            <a
+              href="#"
+              class=" my-4 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                ">
+              Explore Now!
+            </a>
+          </div>
+            <div>
+            <img
+              src="../assets/image/gambar-pemandangan-indonesia.jpg"
+              alt=""
+              class="w-full h-auto object-cover  rounded-r-[1.875rem]" />
+          </div>
         </div>
-    </div>
     `;
-    }
-    
-    fetchData() {
-        const requestOptions = {
-            method: "POST",
-            redirect: "follow"
-        };
-        fetch('https://comptour-be.vercel.app/api/tourist-attractions/get-all', requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            const images = data.data.slice(0, 4).map(item => item.image);
-            const names = data.data.slice(0, 4).map(item => item.name_place);
-            const descriptions = data.data.slice(0, 4).map(item => item.description);
+  }
 
-            const imgElements = this.shadowRoot.querySelectorAll('img');
-            const nameElements = this.shadowRoot.querySelectorAll('h2');
-            const descriptionElements = this.shadowRoot.querySelectorAll('p');
+  fetchData() {
+    const requestOptions = {
+      method: "POST",
+      redirect: "follow",
+    };
+    fetch(
+      "https://comptour-be.vercel.app/api/tourist-attractions/get-all",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const images = data.data.slice(0, 4).map((item) => item.image);
+        const names = data.data.slice(0, 4).map((item) => item.name_place);
+        const descriptions = data.data
+          .slice(0, 4)
+          .map((item) => item.description);
 
-            imgElements.forEach((img, index) => {
-                img.src = images[index];
-                img.alt = names[index];
-            });
+        const imgElements = this.shadowRoot.querySelectorAll("img");
+        const nameElements = this.shadowRoot.querySelectorAll("h2");
+        const descriptionElements = this.shadowRoot.querySelectorAll("p");
 
-            nameElements.forEach((name, index) => {
-                name.textContent = names[index];
-            });
+        imgElements.forEach((img, index) => {
+          img.src = images[index];
+          img.alt = names[index];
+        });
 
-            descriptionElements.forEach((description, index) => {
-                description.textContent = descriptions[index];
-            });
-            })
-        .catch(error => console.error('Error fetching data:', error));
-    }
+        nameElements.forEach((name, index) => {
+          name.textContent = names[index];
+        });
+
+        descriptionElements.forEach((description, index) => {
+          description.textContent = descriptions[index];
+        });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }
 }
-
 
 customElements.define("content-item", contentLanding);
--- src/component/footer-bar.js
+++ src/component/footer-bar.js
@@ -1,146 +1,29 @@
 class Footer extends HTMLElement {
-        constructor() {
-        super();
-        this.attachShadow({ mode: "open" });
-        }
-    
-        connectedCallback() {
-        this.render();
-        }
-    
-        render() {
-        this.shadowRoot.innerHTML = `
-                <footer>
-                    <div class="container">
-                        <div class="row">
-                            <div class="footer-col">
-                                <h4>Resources</h4>
-                                <ul>
-                                    <li><a href="">Flowbite</a></li>
-                                    <li><a href="">TailwindCSS</a></li>
-                                </ul>
-                            </div>
-            
-                            <!-- 2nd Column -->
-                            <div class="footer-col">
-                                <h4>Follow Us</h4>
-                                <ul>
-                                    <li><a href="">Github</a></li>
-                                </ul>
-                            </div>
-            
-                            <!-- 3rd Column -->
-                            <div class="footer-col">
-                                <h4>About</h4>
-                                <ul>
-                                    <li><a href="">Team</a></li>
-                                </ul>
-                            </div>
-            
-                            <!-- 3th Column -->
-            
-                            <div class="footer-col">
-                                <img src="/src/assets/logo.png" alt="">
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col">
-                                <p>&#169; 2024 CompTour. All Rights Reserved.</p>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-                <style>
-                    @import url('https://fonts.googleapis.com/css2?family=Poppins&family=Ubuntu&display=swap');
-                    *{
-                        padding: 0;
-                        margin: 0;
-                        box-sizing: border-box;
-                        font-family: 'Poppins', sans-serif;
-                    }
-                    footer .container{
-                        background-color: rgb(15, 15, 15);
-                    }
-                    
-                    .row{
-                        display: flex;
-                        justify-content: space-between;
-                        padding: 40px 100px;
-                    }
-                    .footer-col ul{
-                        list-style: none;
-                    }
-                    
-                    .footer-col h4{
-                        color: white;
-                        margin-bottom: 25px;
-                        font-size: 22px;
-                        position: relative;
-                        font-family: 'Ubuntu', sans-serif;
-                    }
-                    
-                    .footer-col h4::before{
-                        content: '';
-                        width: 70px;
-                        height: 2px;
-                        position: absolute;
-                        background-color: rgb(233, 165, 40);
-                        bottom: -10px;
-                    }
-                    
-                    .footer-col ul li{
-                        padding: 10px 0;
-                    }
-                    .footer-col ul li a{
-                        color: white;
-                        text-decoration: none;
-                        font-size: 16px;
-                        opacity: 0.7;
-                        transition: 0.5s;
-                    }
-                    .footer-col ul li a:hover{
-                        opacity: 1;
-                    }
-                    .footer-col img {
-                        width: 10rem;
-                    }
-                    .col p{
-                        color: white;
-                    }
-                    .row hr{
-                        opacity: 0.7;
-                    }
-                    
-                    .row .socialIcons i{
-                        display: inline-block;
-                        color: white;
-                        font-size: 20px;
-                        margin: 0 15px;
-                        transition: 0.5s;
-                    }
-                    
-                    .row .socialIcons i:hover{
-                        color: #e91e64;
-                    }
-                    
-                    @media (max-width: 600px){
-                        footer .row{
-                            flex-direction: column;
-                            padding: 20px 30px;
-                        }
-                    
-                        footer .footer-col ul{
-                            margin-left: 20px;
-                            margin-bottom: 20px;
-                        }
-                        .footer-col input{
-                            width: 100%;
-                        }
-                    }
-                </style>
-            `;
-        }
-    }
-    
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+<style>
+     @import "/src/styles/tailwindcss-output/tailwind.css";
+</style>
+<footer class="bg-[#043420] shadow ">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center ">© 2024 <a href="" class="hover:underline">CompTour</a>. All Rights Reserved.
+    </span>
+    <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+        <img src="../assets/logo.png" class="h-16" alt="CompTour Logo" /></a>
+    </div>
+</footer>
+
+    `;
+  }
+}
+
 customElements.define("footer-bar", Footer);
