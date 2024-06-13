ace29df0e041d0f421452b74daa9922076ab9bc9
jihan <jihandatuslanibrahim@gmail.com>
2024-06-13 19:33:54 +0700

    modifikasi konfig tailwind dan tampilan detail blog
    -modifikasi konfigurasi tailwind
    -memperbaiki tampilan detail
    -modifikasi landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
     extend: {
       spacing: {
-        '98': '400px',
-        '100': '450px',
-        '110': '500px',
-        '115': '550px',
-        '120': '600px',
-      }
+        98: "400px",
+        100: "450px",
+        110: "500px",
+        115: "550px",
+        120: "600px",
+      },
     },
   },
   plugins: [],
-}
-
+};
